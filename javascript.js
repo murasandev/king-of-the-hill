fe2d11800c83c7438af89137ff141924135ebafb
Dylan M <dmurayama93@gmail.com>
2026-03-26 12:17:17 -1000

    create function to set random cpu number to rock paper or scissor

--- javascript.js
+++ javascript.js
@@ -9,40 +9,70 @@
 // rock paper scissors
 // best of 3 is the king
 
-
-
-
-
 // test userInput = rock paper or scissors
 // if equal than continue
 // else re ask prompt
-let validUserInput = false;
+let validUserInput;
 let userInput;
+let computerChoice;
 
-// check userInput is valid
-do {
-  userInput = prompt("Rock, Paper, or Scissor");
-  setUserInputToLower(userInput);
-  
-  switch (userInput) {
-    case "rock":
-      console.log("You chose rock");
-      validUserInput = true;
-      break;
-    case "paper":
-      console.log("you chose paper");
-      validUserInput = true;
-      break;
-    case "scissor":
-      console.log("you chose scissor");
-      validUserInput = true;
-      break;
-    default:
-      console.log("Please enter valid input.");
-      break;
-  }
-} while (!validUserInput);
+// Gameplay Loop
+getUserInput();
+getCpuInput();
+setCpuInput();
+
+function getUserInput(){
+  do {
+    userInput = prompt("Rock, Paper, or Scissor");
+    setUserInputToLower(userInput);
+    
+    switch (userInput) {
+      case "rock":
+        console.log("You chose rock");
+        validUserInput = true;
+        break;
+      case "paper":
+        console.log("you chose paper");
+        validUserInput = true;
+        break;
+      case "scissor":
+        console.log("you chose scissor");
+        validUserInput = true;
+        break;
+      default:
+        console.log("Please enter valid input.");
+        break;
+    }
+  } while (!validUserInput);
+}
 
 function setUserInputToLower(string){
   userInput = string.toLowerCase();
+}
+
+function getCpuInput(max = 3){
+  computerChoice = Math.floor(Math.random() * max);
+  console.log(computerChoice);
+}
+
+function setCpuInput(){
+  switch (computerChoice) {
+    case 0:
+      // rock
+      console.log("cpu rock");
+      break;
+    
+    case 1:
+      // paper
+      console.log("cpu paper");
+      break;
+
+    case 2:
+      // scissor
+      console.log("cpu scissor");
+      break;
+
+    default:
+      break;
+  }
 }
