c9a455a1c9fb328fa4429c2e47c64d29f9c9a19e
Dylan M <dmurayama93@gmail.com>
2026-03-26 10:34:04 -1000

    add pseudocode for game logic

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// objective of king of the hill
+// be the last man standing
+
+// start with 3 players
+// rock paper scissor until oddman eliminated
+// ex: two rocks one scissor / scissor is eliminated
+
+// once down to 2 players
+// rock paper scissors
+// best of 3 is the king
